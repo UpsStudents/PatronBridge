3c90cd72d9c7f410f86e3175cf4b5da85b3efd87
mauri <mauricioromero@outlook.com>
2022-10-22 13:38:57 -0500

    corrección paquetes y main

--- main.java
+++ main.java
@@ -3,26 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package p61;
+package p61.bridge;
 
-import p61.adapter.Adapter;
-import p61.bridge.InscripcionGrado;
-import p61.bridge.InscripcionPresencialImpl;
-import p61.bridge.NivelEstudio;
-import p61.composite.Circulo;
-import p61.composite.Dibujo;
-import p61.composite.Linea;
-import p61.decorator.NotificacionConEmail;
-import p61.decorator.NotificacionDecorator;
-import p61.decorator.NotificacionHTML;
-import p61.facade.PersonaFacade;
-import p61.facade.TipoPersona;
 
 /**
  *
  * @author Mauricio Ortiz
  */
-public class PdsUnidad02Ooms {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
